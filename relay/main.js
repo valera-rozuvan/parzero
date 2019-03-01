2785ca3fcc3ae55b65589d24a660ff48ecd07c0f
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-03-01 18:45:03 +0200

    Limit move generation by time, not by depth.

--- relay/main.js
+++ relay/main.js
@@ -19,14 +19,12 @@ const wss = new WebSocket.Server({ port: socketPort });
 console.log(`WebSocket server running at http://127.0.0.1:${socketPort}/`);
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+  ws.on('message', async function incoming(message) {
     if (message === 'ping') {
       ws.send('pong');
     } else if (message.includes('_MOVES_')) {
       const movesObjStr = message.replace('_MOVES_', '');
       const movesObj = JSON.parse(movesObjStr);
-
-      let sideToMove = 'white';
 
       const moves = movesObj.map((move) => {
         if (move[2].length === 0) {
@@ -46,72 +44,150 @@ wss.on('connection', function connection(ws) {
 
       const nMovesToPonder = 15;
 
+      // const engine = new Engine(enginePath)
+      // await engine.init()
+      // await engine.isready()
+      // await engine.setoption('MultiPV', '3')
+      // const emitter = engine.goInfinite()
+      // emitter.on('data', a => {
+      //   console.log('data', a)
+      // })
+
+
       const engine = new Engine(enginePath);
-      engine.chain()
-        .init()
-        .setoption('Threads', 5)
-        .setoption('MultiPV', 5)
-        .position(fenStr)
-        .go({ depth: nMovesToPonder })
-        .then((result) => {
-          // console.log('Engine result: ', JSON.stringify(result));
-          console.log(`---> best move: ${result.bestmove}`);
 
-          let actualDepth = -1;
-          result.info.forEach((infoItem) => {
-            const currentDepth = parseInt(infoItem.depth);
+      await engine.init()
+      await engine.isready()
 
-            if (currentDepth > actualDepth) {
-              actualDepth = currentDepth;
-            }
-          });
+      await engine.setoption('Threads', 5)
+      await engine.setoption('MultiPV', 5)
+      await engine.position(fenStr);
 
-          console.log(`---> depth: ${actualDepth}`);
-          console.log(`---> side to move: ${sideToMove}`);
-          console.log('\n');
+      const emitter = engine.goInfinite();
 
-          result.info.forEach((infoItem) => {
-            if (parseInt(infoItem.depth) !== actualDepth) {
-              return;
-            }
+      emitter.on('data', (result) => {
+        console.log('---------------------------------------');
+        console.log('result');
+        console.log(result);
+        console.log('\n\n\n');
+      });
 
-            if (!infoItem.score) {
-              return;
-            }
+      setTimeout(async () => {
+        const result = await engine.stop();
+        outputResult(result);
+        await engine.quit();
+      }, 300);
 
-            let score = parseInt(infoItem.score.value);
-            let unit = infoItem.score.unit;
+        // .go({ depth: nMovesToPonder })
+        // .then((result) => {
+        //   // console.log('Engine result: ', JSON.stringify(result));
+        //   console.log(`---> best move: ${result.bestmove}`);
 
-            if (isNaN(score)) {
-              console.log('!!!!!!!!!!!!!! score === NaN');
-              score = 0;
-            }
+        //   let actualDepth = -1;
+        //   result.info.forEach((infoItem) => {
+        //     const currentDepth = parseInt(infoItem.depth);
 
-            score *= 0.01;
+        //     if (currentDepth > actualDepth) {
+        //       actualDepth = currentDepth;
+        //     }
+        //   });
 
-            // if (sideToMove === 'white') {
-            //   score *= -1;
-            // }
+        //   console.log(`---> depth: ${actualDepth}`);
+        //   console.log(`---> side to move: ${sideToMove}`);
+        //   console.log('\n');
 
-            console.log(`---> pv: ${infoItem.pv}`);
-            console.log(`---> unit: ${unit}`);
-            console.log(`---> score: ${score}`);
-            console.log('\n');
-          });
+        //   result.info.forEach((infoItem) => {
+        //     if (parseInt(infoItem.depth) !== actualDepth) {
+        //       return;
+        //     }
 
-          console.log('\n\n\n');
+        //     if (!infoItem.score) {
+        //       return;
+        //     }
 
-          engine.quit();
-        })
-        .catch((e) => {
-          console.log(e);
-          engine.quit();
-        });
+        //     let score = parseInt(infoItem.score.value);
+        //     let unit = infoItem.score.unit;
+
+        //     if (isNaN(score)) {
+        //       console.log('!!!!!!!!!!!!!! score === NaN');
+        //       score = 0;
+        //     }
+
+        //     score *= 0.01;
+
+        //     // if (sideToMove === 'white') {
+        //     //   score *= -1;
+        //     // }
+
+        //     console.log(`---> pv: ${infoItem.pv}`);
+        //     console.log(`---> unit: ${unit}`);
+        //     console.log(`---> score: ${score}`);
+        //     console.log('\n');
+        //   });
+
+        //   console.log('\n\n\n');
+
+        //   engine.quit();
+        // })
+        // .catch((e) => {
+        //   console.log(e);
+        //   engine.quit();
+        // });
     }
   });
 
   ws.send('something');
 });
+
+function outputResult(result) {
+  let sideToMove = 'white';
+
+  console.log(`---> best move: ${result.bestmove}`);
+
+  let actualDepth = -1;
+  result.info.forEach((infoItem) => {
+    const currentDepth = parseInt(infoItem.depth);
+
+    if (currentDepth > actualDepth) {
+      actualDepth = currentDepth;
+    }
+  });
+
+  console.log(`---> depth: ${actualDepth}`);
+  console.log(`---> side to move: ${sideToMove}`);
+  console.log('\n');
+
+  result.info.forEach((infoItem) => {
+    if (parseInt(infoItem.depth) !== actualDepth) {
+      return;
+    }
+
+    if (!infoItem.score) {
+      return;
+    }
+
+    let score = parseInt(infoItem.score.value);
+    let unit = infoItem.score.unit;
+
+    if (isNaN(score)) {
+      console.log('!!!!!!!!!!!!!! score === NaN');
+      score = 0;
+    }
+
+    score *= 0.01;
+
+    // if (sideToMove === 'white') {
+    //   score *= -1;
+    // }
+
+    console.log(`---> pv: ${infoItem.pv}`);
+    console.log(`---> unit: ${unit}`);
+    console.log(`---> score: ${score}`);
+    console.log('\n');
+  });
+
+  console.log('\n\n\n');
+}
 
 function getFenPosition(moveHistory) {
   const chessInstance = new Chess();
